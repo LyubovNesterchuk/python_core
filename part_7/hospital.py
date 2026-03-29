223b890f276a9dcb330dae44d88ad63f0b2aecf5
LyubovNesterchuk <nesterchuklyubov@gmail.com>
2026-03-29 18:19:48 +0300

    lesson

---
+++ part_7/hospital.py
@@ -0,0 +1,120 @@
+''' Є лікарня, в якій є Лікарі та Пацієнти. У кожного Пацієнта є Історія_хвороб\
+В Історії_хвороб зберігається 10 останніх Хвороб.\
+Хвороба містить інформацію про те, коли пацієнт захворів, хто його лікував, коли одужав (якщо одужав),\
+процедури та лікування, які призначив лікар.\
+Лікар може призначати процедури (наприклад діагностичні) та лікування (наприклад курс ліків).
+У лікарів і пацієнтів є Ім'я та Прізвище. У пацієнтів є номер поліса соціального страхування.
+У лікарів є посада. Пацієнт не може змінювати свою Історію_хвороб.
+Завдання: реалізувати в класах наведені вище умови.'''
+
+from datetime import datetime
+from collections import UserList, deque
+
+class Person:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+class NoRight(Exception):
+    pass
+
+class Disease:
+    def __init__(self, name, doctor):
+        self.name = name
+        self.doctor = doctor
+        self.treatment = list()
+        self.time = datetime.now()
+        self.cured = False
+        self.cure_time = None
+
+    def cure(self):
+        self.cured = True
+        self.cure_time = datetime.now()
+
+    def treat(self, treatment):
+        self.treatment.append(treatment)
+
+class Story(UserList):
+    story_size = 10
+
+    def __init__(self):
+        self.__data = deque([], self.story_size)
+        self.__data__dict = dict()
+        self.data = list()
+
+    def __getitem__(self, key=None):
+        if key is None:
+            return list(self.__data)
+        if isinstance(key, int):
+            return self.__data[key]
+        elif isinstance(key, str):
+            return self.__data__dict.get(key)
+
+    def __len__(self):
+        return len(self.__data)
+
+    def add(self, *_):
+        raise NoRight
+
+    def __add(self, disease):
+        self.__data.append(disease)
+        self.__data__dict = {des.name: des for des in self.__data}
+        self.data = [des for des in self.__data]
+
+    @property
+    def healthy(self):
+        return all([disease.cured for disease in self.__data])
+
+class Patient(Person):
+    def __init__(self, name, surname, polis):
+        super().__init__(name, surname)
+        self.polis = polis
+        self.story = Story()
+
+    @property
+    def healthy(self):
+        return self.story.healthy
+
+class Doctor(Person):
+    def __init__(self, name, surname, position):
+        super().__init__(name, surname)
+        self.position = position
+
+    def add(self, patient, disease_name):
+        des = Disease(disease_name, ' '.join((self.name, self.surname)))
+        patient.story._Story__add(des)
+
+    def treat(self, patient, disease_name, treatment):
+        patient.story[disease_name].treat(treatment)
+
+
+
+doctor_surg = Doctor('Bill', "Klinton", "Head")
+doctor_tera = Doctor('Tom', 'Jerry', 'Therapist')
+
+patient_one = Patient("George", 'Bush', 123456789)
+patient_two = Patient('Barak', 'Obama', 987654321)
+
+doctor_tera.add(patient_one, 'flu')
+doctor_surg.add(patient_two, 'appendicit')
+
+print(patient_one.story['flu'].time)
+print(patient_two.story['appendicit'].time)
+
+doctor_tera.treat(patient_one, 'flu', 'medicine')
+doctor_surg.treat(patient_two, 'appendicit', 'operation')
+
+print(patient_one.story['flu'].treatment)
+print(patient_two.story['appendicit'].treatment)
+
+print(patient_one.healthy)
+
+patient_one.story['flu'].cure()
+print(patient_one.healthy)        
+
+'''2026-03-29 18:09:21.329589
+2026-03-29 18:09:21.329589
+['medicine']
+['operation']
+False
+True'''
---
+++ part_7/order.py
@@ -0,0 +1,162 @@
+'''Створіть класи
+1. `item`
+2. `order`-- містить список `items`
+3. `customer`-- може переглядати товари по порядку
+4. `seller`-- може додати/видалити товар у замовлення і підтвердити замовлення (відправити виробнику)
+5. `manufacturer` -- може додати замовлення у внутрішній список замовлень, показати стан готовності (`True`/`False`) для замовлення
+і встановити замовлення як завершене'''
+
+
+from uuid import uuid1
+
+
+class NotEnoughRights(Exception):
+    def __init__(self, *value):
+        self.value = value
+
+    def __str__(self):
+        return 'You have not enough rights for this' + repr(self.value)
+
+
+class Item:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class Order:
+    def __init__(self, id_=None):
+        if id_ is None:
+            id_ = uuid1().int
+        self.id = id_
+        self.items = list()
+        self.total = 0
+
+    def __add__(self, item):
+        self.items.append(item)
+        self.total += item.price
+
+    def __sub__(self, item):
+        if item in self.items:
+            self.items.remove(item)
+            self.total -= item.price
+
+    def __str__(self):
+        msg = "Items\n"
+        if self.items:
+            for item in self.items:
+                msg += f"{item.name}:{item.price}\n"
+        msg += f'Total: {self.total}'
+        return msg
+
+
+class Person:
+
+    def add(self, *args):
+        raise NotEnoughRights
+
+    def sub(self, *args):
+        raise NotEnoughRights
+
+    def view(self, *args):
+        raise NotEnoughRights
+
+    def confirm(self, *args):
+        raise NotEnoughRights
+
+    def complete(self, *args):
+        raise NotEnoughRights
+
+
+class Customer(Person):
+    def view(self, order):
+        return str(order)
+
+
+class Seller(Person):
+    def add(self, item, order=None):
+        if order is None:
+            order = Order()
+        order + item
+        return order
+
+    def sub(self, item, order):
+        order - item
+
+    def view(self, order):
+        return str(order)
+
+    def confirm(self, order, manufacturer):
+        manufacturer.add(order)
+
+
+class Manufacturer(Person):
+    def __init__(self):
+        self.orders = dict()
+
+    @property
+    def order_list(self):
+        return list(self.orders.keys())
+
+    def add(self, order):
+        self.orders[order.id] = False
+
+    def view(self, order):
+        if order.id in self.orders:
+            return self.orders[order.id]
+
+    def complete(self, order):
+        if order.id in self.orders:
+            self.orders[order.id] = True
+
+
+
+ball = Item("Ball", 10)
+cup = Item("Cup", 5)
+bag = Item("Bag", 15)
+
+seller = Seller()
+customer = Customer()
+manufacturer = Manufacturer()
+
+new_order = seller.add(ball)
+print(seller.view(new_order))
+print(customer.view(new_order))
+
+seller.add(cup, new_order)
+print(customer.view(new_order))
+
+seller.sub(cup, new_order)
+print(customer.view(new_order))
+
+seller.add(bag, new_order)
+print(customer.view(new_order))
+
+seller.confirm(new_order, manufacturer)
+print(manufacturer.order_list)
+print(manufacturer.view(new_order))
+manufacturer.complete(new_order)
+print(manufacturer.view(new_order))
+
+
+
+'''Items    
+Ball:10  
+Total: 10
+Items  
+Ball:10
+Total: 10
+Items
+Ball:10
+Cup:5
+Total: 15
+Items
+Ball:10
+Total: 10
+Items
+Ball:10
+Bag:15
+Total: 25
+[115405577248266781233449744465832305613]
+False
+True'''
